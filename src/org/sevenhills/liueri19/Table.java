236fc8f10b57a2f1d2d5944f7dfc982d4887b0c9
liueri19 <liueri19@7hills.org>
2016-10-18 13:14:02 -0400

    Implement game display refresh-Untested

--- src/org/sevenhills/liueri19/Table.java
+++ src/org/sevenhills/liueri19/Table.java
@@ -1,6 +1,7 @@
 package org.sevenhills.liueri19;
 
-public class Table {
+public class Table extends JPanel implements ActionListener {
+	private final Timer timer = new Timer(17, this);	//approximately 60 fps
 	public final int width, height;
 	public double velocity = 1;	//in pixels/millisecond? IDK yet...
 	//private List<Ball> balls = new ArrayList<Ball>();	may add multiple balls for difficulty (just a thought)
@@ -16,8 +17,20 @@ public class Table {
 	}
 	
 	public Table(int width, int height) {
+		super(true);
 		this.width = width;
 		this.height = height;
+		this.setPreferredSize(new Dimension(width, height));
+	}
+	
+	public static void main(String[] args) {
+		Table table = new Table();
+		JFrame frame = new JFrame("Pong Game");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	f.add(table);
+        	f.pack();
+		f.setVisisble(true);
+		timer.start();
 	}
 	
 	public void resetBall() {
@@ -25,13 +38,29 @@ public class Table {
 		ball.setCoordinate(width / 2, height / 2);
 	}
 	
-	public void setBall(double direction) {
+	//invoked on every repaint()
+	@Override
+	public void paintComponent(Graphics g) {
+		//paint table
 		
+		//paint ball
+		
+		//paint paddles
+		
+		//paint scores
+		
+	}
+	
+	//will be invoked by the timer with 20 milliseconds intervals
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.repaint();
 	}
 	
 	//accessors & mutators
 	public void playerLScore() {
 		playerLScore++;
+		resetBall();
 	}
 	
 	public int getPlayerLScore() {
@@ -40,6 +69,7 @@ public class Table {
 	
 	public void playerRScore() {
 		playerRScore++;
+		resetBall();
 	}
 	
 	public int getPlayerRScore() {
